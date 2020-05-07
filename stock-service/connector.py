80639cabce39dec0c79f283324b22bee8268bbf5
Sayra Ranjha <S.S.Ranjha@student.tudelft.nl>
2020-05-07 16:21:13 +0200

    Implement ScyllaDB connector and endpoint functionality for stock service

--- stock-service/connector.py
+++ stock-service/connector.py
@@ -1,20 +1,53 @@
 from cassandra.cluster import Cluster
-from cassandra.cqlengine import connection
+from cassandra.cqlengine import connection, ValidationError
 from cassandra.cqlengine.management import sync_table
+from cassandra.cqlengine.query import QueryException
 
-from item import Item
-
-session = Cluster(['127.0.0.1']).connect()
-session.execute("""
-    CREATE KEYSPACE IF NOT EXISTS wdm
-    WITH replication = { 'class': 'SimpleStrategy', 'replication_factor': '2' }
-    """)
-
-connection.setup(['127.0.0.1'], "wdm")
+from .item import Item
 
 
-sync_table(Item)
+class ScyllaConnector:
+    def __init__(self):
+        session = Cluster(['127.0.0.1']).connect()
+        session.execute("""
+            CREATE KEYSPACE IF NOT EXISTS wdm
+            WITH replication = { 'class': 'SimpleStrategy', 'replication_factor': '2' }
+            """)
 
-item1 = Item.create(price=10, count=1)
+        connection.setup(['127.0.0.1'], "wdm")
+        sync_table(Item)
 
-print(item1)
+    @staticmethod
+    def create_item(price):
+        item = Item.create(price=price, count=0)
+        return item.id
+
+    @staticmethod
+    def get_item(item_id):
+        try:
+            item = Item.get(id=item_id)
+        except QueryException:
+            raise ValueError(f"Item with id {item_id} not found")
+        except ValidationError:
+            raise ValueError(f"Item id {item_id} is not a valid id")
+
+        return item
+
+    def add_amount(self, item_id, number):
+        item = self.get_item(item_id)
+        item.count = item.count + number
+        Item.update(item)
+        return item.count
+
+    def subtract_amount(self, item_id, number):
+        item = self.get_item(item_id)
+        item.count = item.count - number
+
+        assert item.count >= 0, 'Item count cannot be negative'
+
+        Item.update(item)
+        return item.count
+
+    def get_availability(self, item_id):
+        item = self.get_item(item_id)
+        return item.count
--- stock-service/stock-service.py
+++ stock-service/stock-service.py
@@ -1,26 +1,46 @@
-from flask import Flask
+from flask import Flask, abort
 from flask import request
 from markupsafe import escape
+from .connector import ScyllaConnector
 
 app = Flask(__name__)
+connector = ScyllaConnector()
 
 
 @app.route('/stock/availability/<item_id>', methods=['GET'])
 def get_availability(item_id):
-    pass
+    try:
+        item_count = connector.get_availability(escape(item_id))
+        return str(item_count)
+    except ValueError:
+        abort(404)
 
 
-@app.route('/stock/subtract/<item_id>/<number>', methods=['POST'])
-def subtract_item(item_id, number):
-    pass
+@app.route('/stock/subtract/<item_id>/<int:number>', methods=['POST'])
+def subtract_amount(item_id, number):
+    try:
+        item_count = connector.subtract_amount(item_id, number)
+        return str(item_count)
+    except AssertionError:
+        abort(400)
+    except ValueError:
+        abort(404)
 
 
-@app.route('/stock/add/<item_id>/<number>', methods=['POST'])
-def add_item(item_id, number):
-    pass
+@app.route('/stock/add/<item_id>/<int:number>', methods=['POST'])
+def add_amount(item_id, number):
+    try:
+        item_count = connector.add_amount(item_id, number)
+        return str(item_count)
+    except ValueError:
+        abort(404)
 
 
 @app.route('/stock/create', methods=['POST'])
 def create_item():
-    pass
-
+    try:
+        price = int(escape(request.form['price']))
+        item_id = connector.create_item(price)
+        return str(item_id)
+    except:
+        abort(500)
