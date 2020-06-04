7a6c92fd1c8183ef1c4cc310b8ba508288ed5d53
Olivia <oliverhuang714@gmail.com>
2020-06-04 15:36:50 +0200

    Update postgress_order.py
    Set postgres_order with two primary key

--- order_service/postgress_order.py
+++ order_service/postgress_order.py
@@ -11,7 +11,7 @@ class PostgresOrder(Base_order):
     __tablename__ = 'order'
 
     order_id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, unique=True, nullable=False)
-    user_id = Column(UUID(as_uuid=True), primary_key=False, default=uuid.uuid4, unique=True, nullable=False)
+    user_id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, unique=True, nullable=False)
     paid = Column(Boolean, nullable=False)
 
     def __init__(self, user_id):
