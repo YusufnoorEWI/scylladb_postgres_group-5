bef815cd3d38801f9d507d1a90fa3f567ab0ff74
Yusufnoor <yusufnoor2997@gmail.com>
2020-06-05 17:59:35 +0200

    Add db type environment variable to docker file Change table name and references

--- users_service/postgres_user.py
+++ users_service/postgres_user.py
@@ -8,7 +8,7 @@ Base = declarative_base()
 
 
 class PostgresUser(Base):
-    __tablename__ = 'stock_item'
+    __tablename__ = 'user'
 
     id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, unique=True, nullable=False)
     credit = Column(Numeric, nullable=False)
