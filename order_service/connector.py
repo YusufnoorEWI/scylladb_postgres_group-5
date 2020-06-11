d39c27da19f44204d1fadb510e09ca850509228b
Sayra Ranjha <S.S.Ranjha@student.tudelft.nl>
2020-06-11 17:19:02 +0200

    Create Scylla cluster for order service

--- order_service/connector.py
+++ order_service/connector.py
@@ -1,6 +1,7 @@
 import os
 from time import sleep
 
+from cassandra import ConsistencyLevel
 from cassandra.cluster import Cluster
 from cassandra.cqlengine import connection, ValidationError
 from cassandra.cqlengine.management import sync_table
@@ -25,6 +26,7 @@ class ConnectorFactory:
         self.postgres_password = os.getenv('POSTGRES_PASSWORD', 'mysecretpassword')
         self.postgres_port = os.getenv('POSTGRES_PORT', '5432')
         self.postgres_name = os.getenv('POSTGRES_DB', 'postgres')
+        self.scylla_nodes = os.getenv('SCYLLA_NODES').split(" ")
 
     def get_connector(self):
         """
@@ -37,27 +39,29 @@ class ConnectorFactory:
             return PostgresConnector(self.postgres_user, self.postgres_password, self.db_host, self.postgres_port,
                                      self.postgres_name)
         elif self.db_type == 'scylla':
-            return ScyllaConnector(self.db_host)
+            return ScyllaConnector(self.scylla_nodes)
         else:
             raise ValueError("Invalid database")
 
 
 class ScyllaConnector:
-    def __init__(self, host):
+    def __init__(self, nodes):
         """Establishes a connection to the ScyllaDB database, creates the "wdm" keyspace if it does not exist
         and creates or updates the stock_item table.
         """
         while True:
             try:
-                session = Cluster([host]).connect()
+                session = Cluster(contact_points=nodes).connect()
                 break
             except Exception:
                 sleep(1)
         session.execute("""
             CREATE KEYSPACE IF NOT EXISTS wdm
-            WITH replication = { 'class': 'SimpleStrategy', 'replication_factor': '2' }
+            WITH replication = { 'class': 'SimpleStrategy', 'replication_factor': '3' }
             """)
-        connection.setup([host], "wdm")
+        session.default_consistency_level = ConsistencyLevel.QUORUM
+
+        connection.setup(nodes, "wdm")
         sync_table(ScyllaOrderItem)
         sync_table(ScyllaOrder)
         # Also sync the order table, retrieve everything.
